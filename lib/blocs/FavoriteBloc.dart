6d47934bac6c58ba9969c61e8223313a7450519d
israelhudson <israelhudson@gmail.com>
2019-06-30 19:36:34 -0300

    criando variaveis

--- lib/blocs/FavoriteBloc.dart
+++ lib/blocs/FavoriteBloc.dart
@@ -1,13 +1,17 @@
+import 'package:flutter_favorites_bloc/models/Post.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 
 class FavoriteBloc extends BlocBase{
 
+  final _favoritesListController = BehaviorSubject<List<Post>>();
+  Observable<List<Post>> get favoritesListFlux => _favoritesListController.stream;
+  Sink<List<Post>> get favoritesListEvent => _favoritesListController.sink;
 
-
+  final _favoritesListAddController = BehaviorSubject<>
   @override
   void dispose() {
-    // TODO: implement dispose
+    _favoritesListController.close();
   }
 
 }

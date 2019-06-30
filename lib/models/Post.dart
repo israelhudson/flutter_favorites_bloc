ab13671d1ba6b27a94d65625dc535a358741e794
israelhudson <israelhudson@gmail.com>
2019-06-30 00:28:43 -0300

    preparando as models e services de post

---
+++ lib/models/Post.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'Post.g.dart';
+//flutter packages run build_runner build
+
+@JsonSerializable()
+class Post {
+  int userId;
+  int id;
+  String title;
+  String body;
+
+  Post();
+
+  factory Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
+
+  Map<String, dynamic> toJson() => _PostToJson(this);
+}

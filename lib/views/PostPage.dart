9ba1aa38c03b8711297a0c5789bec51c50d82569
Israel Hudson <israelhudson@gmail.com>
2019-07-01 09:40:06 -0300

    exibindo dados na view

--- lib/views/PostPage.dart
+++ lib/views/PostPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_favorites_bloc/blocs/PostBloc.dart';
+import 'package:flutter_favorites_bloc/models/Post.dart';
 
 class PostPage extends StatefulWidget {
   @override
@@ -6,6 +8,20 @@ class PostPage extends StatefulWidget {
 }
 
 class _PostPageState extends State<PostPage> {
+  PostBloc _postBloc;
+
+  @override
+  void initState() {
+    _postBloc = new PostBloc();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _postBloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +30,29 @@ class _PostPageState extends State<PostPage> {
         title: Text("Pagina de posts"),
       ),
       body: Container(
-        child: Center(
-          child: Text("Post page"),
-        ),
+        child: StreamBuilder(
+            stream: _postBloc.postListFlux,
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
+              if(snapshot.hasData){
+                return ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    Post post = snapshot.data[index];
+                    return Container(
+                      width: 200,
+                      height: 200,
+                      child: Center(
+                        child: Text(post.title),
+                      ),
+                    );
+                  },
+                );
+              }else{
+                Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }),
       ),
     );
   }
